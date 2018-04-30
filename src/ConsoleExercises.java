5c1a77afd175373b4c2f242e7063f9420b4ac19e
Noah Dela Cruz <noahjdc92@gmai.com>
2018-04-30 16:04:29 -0500

    cleaned up code and fix some spelling errors

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -10,7 +10,7 @@ public class ConsoleExercises {
         myNumber = sc.nextInt();
         System.out.println();
 
-        //When you enter less that 3 words, the system will not provide an out until three words have been made.
+        //When you enter less than 3 words, the system will not provide an out until three words have been made.
         String wordOne;
         String wordTwo;
         String wordThree;
@@ -23,21 +23,22 @@ public class ConsoleExercises {
         System.out.println(wordThree);
         System.out.println();
 
-
+        sc.nextLine();
         //when using the .next() method, it only returns the first word
         //nextLine is finishing the code only when using .useDelimeter("\n") on the scanner.
         String sentence;
         System.out.println("Please enter a sentence: ");
-        sentence = sc.next();
+        sentence = sc.nextLine();
         System.out.println(sentence);
 
 
 
         //When converting string to int, need to convert to parseInt
-        String lengthString = sc.next();
-        String widthString = sc.next();
+        System.out.println();
         System.out.println("Please enter the length and width of the class: ");
+        String lengthString = sc.next();
         int length =Integer.parseInt(lengthString);
+        String widthString = sc.next();
         int width =Integer.parseInt(widthString);
         System.out.println("area = " + (length*width));
         System.out.println("perimeter = " + (2*length) + (2*width));
