53315b97e96c8342d1ff8f418dfb1b5aa96f1af8
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-01 09:57:16 -0500

    Performed fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,15 +3,28 @@ public class ControlFlowExercises {
         //LOOP BASICS
 
 
-        for(int i = 0; i<=15; i++){
+        for (int i = 5; i <= 15; i++) {
             System.out.println(i);
         }
 
 
         //continually passing 0s after 65536
-        for(int x = 2; x<=65536; x*=x){
+        for (int x = 2; x <= 100; x *= x) {
             System.out.println(x);
         }
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(String.valueOf(i));
+            }
+        }
+
     }
 }
 
