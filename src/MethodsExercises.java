8f5ecde370f77b764e7f1e77aacf813b4c571a2d
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-02 09:47:06 -0500

    made basic arithmetic/ with modulus

---
+++ src/MethodsExercises.java
@@ -0,0 +1,24 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(addition(2, 4));
+        System.out.println(subtraction(5, 6));
+        System.out.println(multiplication(10, 4));
+        System.out.println(division(2, 6));
+        System.out.println(modulus(4, 15));
+    }
+    public static int addition(int number, int add){
+        return number + add;
+    }
+    public static int subtraction(int number, int reduce){
+        return number - reduce;
+    }
+    public static int multiplication(int number, int times){
+        return number * times;
+    }
+    public static int division(int number, int divide){
+        return number / divide;
+    }
+    public static int modulus(int number, int remainder){
+        return number % remainder;
+    }
+}
