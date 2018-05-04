ebf3179d322adc4f90b772873c4d29da0e9c3eaf
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-04 10:34:04 -0500

    added do while

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -4,11 +4,12 @@ import util.Input;
 public class CircleApp {
     public static void main(String[] args) {
         Input input = new Input();
-        double radius = input.getDouble();
-        Circle circle = new Circle(radius);
-        System.out.println(circle.getArea());
-        System.out.println(circle.getCircumference());
-
+        do {
+            double radius = input.getDouble();
+            Circle circle = new Circle(radius);
+            System.out.println(circle.getArea());
+            System.out.println(circle.getCircumference());
+        } while(input.yesNo());
 
     }
 }
