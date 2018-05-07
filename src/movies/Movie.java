27f3eb91e4b3573fa820b8c75856dcab4edb9f9c
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-07 09:58:20 -0500

    added movie array

---
+++ src/movies/Movie.java
@@ -0,0 +1,26 @@
+package movies;
+
+public class Movie {
+    private String name;
+    private String catagory;
+
+    public name(){
+
+    }
+    public catagory(){
+
+    }
+
+    public String getName(){
+
+    }
+    public String setName(){
+
+    }
+    public String getCatagory(){
+
+    }
+    public String setCatagory(){
+
+    }
+}
